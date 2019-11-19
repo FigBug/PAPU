8b6d3e78fd02cd9a876aed517810b7893870fcdb
Roland Rabien <figbug@gmail.com>
2019-11-19 06:34:52 -0800

    Folder names

--- Scripts/pcbuildprojects.bat
+++ Scripts/pcbuildprojects.bat
@@ -5,7 +5,6 @@ cd Scripts
 mkdir bin
 mkdir bin/mac
 
-cd "..\plugin\Builds\Windows"
-xcodebuild -configuration Release || exit 1
+cd "..\plugin\Builds\VisualStudio2017"
 "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" PAPU.sln /p:VisualStudioVersion=15.0 /m /t:Build /p:Configuration=Release /p:Platform=x64 /p:PreferredToolArchitecture=x64  /p:TreatWarningsAsErrors=true
 
--- Scripts/pcresaveprojects.bat
+++ Scripts/pcresaveprojects.bat
@@ -1,6 +1,6 @@
 set ROOT=%cd%
 
-cd "%ROOT%\modules\juce\extras\Projucer\Builds\Windows\"
+cd "%ROOT%\modules\juce\extras\Projucer\Builds\VisualStudio2017\"
 "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" Projucer.sln /p:VisualStudioVersion=15.0 /m /t:Build /p:Configuration=Release /p:Platform=x64 /p:PreferredToolArchitecture=x64  /p:TreatWarningsAsErrors=true
 
 .\build\Release\Projucer.exe --resave "%ROOT%\plugin\PAPU.jucer"
