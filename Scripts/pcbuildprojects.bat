242705fe9f0ad6c234672f8ff7168a2483448fae
Roland Rabien <figbug@gmail.com>
2019-11-19 06:49:38 -0800

    MSBuld path

--- Scripts/pcbuildprojects.bat
+++ Scripts/pcbuildprojects.bat
@@ -1,10 +1,12 @@
+set MSBUILD_EXE=C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe
+
 set ROOT=%cd%
 cd "%ROOT%"
 
 cd Scripts
 mkdir bin
-mkdir bin/mac
+mkdir bin\mac
 
 cd "..\plugin\Builds\VisualStudio2017"
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" PAPU.sln /p:VisualStudioVersion=15.0 /m /t:Build /p:Configuration=Release /p:Platform=x64 /p:PreferredToolArchitecture=x64  /p:TreatWarningsAsErrors=true
+"%MSBUILD_EXE%" PAPU.sln /p:VisualStudioVersion=15.0 /m /t:Build /p:Configuration=Release /p:Platform=x64 /p:PreferredToolArchitecture=x64  /p:TreatWarningsAsErrors=true
 
