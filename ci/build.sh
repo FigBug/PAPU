934763e0bf5d1f1bcefe61fea90949b52f562d6d
Roland Rabien <figbug@gmail.com>
2020-04-28 21:57:11 -0700

    Update build.sh

--- ci/build.sh
+++ ci/build.sh
@@ -131,7 +131,7 @@ if [ "$OS" = "linux" ]; then
   zip -r ${PLUGIN}_Linux.zip $PLUGIN.so
 
   if [ "$BRANCH" = "release" ]; then
-    curl -F "files=@${PLUGIN}_Mac.zip" "https://socalabs.com/files/set.php?key=$APIKEY"
+    curl -F "files=@${PLUGIN}_Linux.zip" "https://socalabs.com/files/set.php?key=$APIKEY"
   fi
 fi
 
