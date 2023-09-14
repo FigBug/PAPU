55b47ef6a1bb6f0eaeb6c1cd70f6840aa826ccf9
Roland Rabien <figbug@gmail.com>
2023-09-13 20:59:34 -0700

    Switch to cmake

--- ci/build.sh
+++ ci/build.sh
@@ -3,13 +3,13 @@
 PLUGIN="PAPU"
 
 # linux specific stiff
-if [ $OS = "linux" ]; then
+if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
   sudo apt-get update
-  sudo apt-get install clang git ladspa-sdk freeglut3-dev g++ libasound2-dev libcurl4-openssl-dev libfreetype6-dev libjack-jackd2-dev libx11-dev libxcomposite-dev libxcursor-dev libxinerama-dev libxrandr-dev mesa-common-dev webkit2gtk-4.0 juce-tools xvfb
+  sudo apt-get install clang git ninja-build ladspa-sdk freeglut3-dev g++ libasound2-dev libcurl4-openssl-dev libfreetype6-dev libjack-jackd2-dev libx11-dev libxcomposite-dev libxcursor-dev libxinerama-dev libxrandr-dev mesa-common-dev webkit2gtk-4.0 juce-tools xvfb
 fi
 
 # mac specific stuff
-if [ $OS = "mac" ]; then
+if [ "$(uname)" == "Darwin" ]; then
   # Create a temp keychain
   if [ -n "$GITHUB_ACTIONS" ]; then
     echo "Create a keychain"
@@ -34,8 +34,6 @@ fi
 ROOT=$(cd "$(dirname "$0")/.."; pwd)
 cd "$ROOT"
 echo "$ROOT"
-rm -Rf bin
-mkdir bin
 
 BRANCH=${GITHUB_REF##*/}
 echo "$BRANCH"
@@ -44,63 +42,28 @@ cd "$ROOT/ci"
 rm -Rf bin
 mkdir bin
 
-# Get the hash
-cd "$ROOT/modules/juce"
-HASH=`git rev-parse HEAD`
-echo "Hash: $HASH"
-
-# Get the Projucer
-cd "$ROOT/ci/bin"
-while true
-do
-  PROJUCER_URL=$(curl -s -S "https://projucer.rabien.com/get_projucer.php?hash=$HASH&os=$OS&key=$APIKEY")
-  echo "Response: $PROJUCER_URL"
-  if [[ $PROJUCER_URL == http* ]]; then
-    curl -s -S $PROJUCER_URL -o "$ROOT/ci/bin/Projucer.zip"
-    unzip Projucer.zip
-    break
-  fi
-  sleep 15
-done
-
-# Resave jucer file
-if [ "$OS" = "mac" ]; then
-  "$ROOT/ci/bin/Projucer.app/Contents/MacOS/Projucer" --resave "$ROOT/plugin/$PLUGIN.jucer"
-elif [ "$OS" = "linux" ]; then
-  "$ROOT/ci/bin/Projucer" --resave "$ROOT/plugin/$PLUGIN.jucer"
-else
-  "$ROOT/ci/bin/Projucer.exe" --resave "$ROOT/plugin/$PLUGIN.jucer"
-fi
-
 # Build mac version
-if [ "$OS" = "mac" ]; then
-  cd "$ROOT/plugin/Builds/MacOSX"
-  xcodebuild -configuration Release || exit 1
+if [ "$(uname)" == "Darwin" ]; then
+  cd "$ROOT"
+  cmake --preset xcode
+  cmake --build --preset xcode --config Release
 
-  cp -R ~/Library/Audio/Plug-Ins/VST/$PLUGIN.vst "$ROOT/ci/bin"
-  cp -R ~/Library/Audio/Plug-Ins/VST3/$PLUGIN.vst3 "$ROOT/ci/bin"
-  cp -R ~/Library/Audio/Plug-Ins/Components/$PLUGIN.component "$ROOT/ci/bin"
+  cp -R "$ROOT/Builds/xcode/${PLUGIN}_artefacts/Release/AU/$PLUGIN.component" "$ROOT/ci/bin"
+  cp -R "$ROOT/Builds/xcode/${PLUGIN}_artefacts/Release/VST/$PLUGIN.vst" "$ROOT/ci/bin"
+  cp -R "$ROOT/Builds/xcode/${PLUGIN}_artefacts/Release/VST3/$PLUGIN.vst3" "$ROOT/ci/bin"
 
   cd "$ROOT/ci/bin"
   codesign -s "$DEV_APP_ID" -v $PLUGIN.vst --options=runtime --timestamp --force
   codesign -s "$DEV_APP_ID" -v $PLUGIN.vst3 --options=runtime --timestamp --force
   codesign -s "$DEV_APP_ID" -v $PLUGIN.component --options=runtime --timestamp --force
 
-  # Build notarize tool
-  cd "$ROOT/modules/gin/tools/notarize"
-  "$ROOT/ci/bin/Projucer.app/Contents/MacOS/Projucer" --set-global-search-path osx defaultJuceModulePath "$ROOT/modules/juce/modules" 
-  "$ROOT/ci/bin/Projucer.app/Contents/MacOS/Projucer" --resave "notarize.jucer"
-  cd Builds/MacOSX
-  xcodebuild -configuration Release || exit 1
-  cd build/Release
-  cp notarize "$ROOT/ci/bin"
-
   # Notarize
   cd "$ROOT/ci/bin"
   zip -r ${PLUGIN}_Mac.zip $PLUGIN.vst $PLUGIN.vst3 $PLUGIN.component
-  cp ${PLUGIN}_Mac.zip "$ROOT/bin"
 
-  "$ROOT/ci/bin/notarize" -ns ${PLUGIN}_Mac.zip $APPLE_USER $APPLE_PASS com.figbug.$PLUGIN.vst
+  if [[ -n "$APPLE_USER" ]]; then
+    xcrun notarytool submit --verbose --apple-id "$APPLE_USER" --password "$APPLE_PASS" --team-id "3FS7DJDG38" --wait --timeout 30m ${PLUGIN}_Mac.zip
+  fi
 
   rm ${PLUGIN}_Mac.zip
   xcrun stapler staple $PLUGIN.vst
@@ -114,20 +77,21 @@ if [ "$OS" = "mac" ]; then
 fi
 
 # Build linux version
-if [ "$OS" = "linux" ]; then
-  cd "$ROOT/plugin/Builds/LinuxMakefile"
-  make CONFIG=Release
+if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+  cd "$ROOT"
+  
+  cmake --preset ninja-gcc
+  cmake --build --preset ninja-gcc --config Release
 
-  cp ./build/$PLUGIN.so "$ROOT/ci/bin"
-  cp -r ./build/$PLUGIN.vst3 "$ROOT/ci/bin"
-  cp -r ./build/$PLUGIN.lv2 "$ROOT/ci/bin"
+  cp -R "$ROOT/Builds/ninja-gcc/${PLUGIN}_artefacts/Release/LV2/$PLUGIN.lv2" "$ROOT/ci/bin"
+  cp -R "$ROOT/Builds/ninja-gcc/${PLUGIN}_artefacts/Release/VST/lib$PLUGIN.so" "$ROOT/ci/bin/$PLUGIN.so"
+  cp -R "$ROOT/Builds/ninja-gcc/${PLUGIN}_artefacts/Release/VST3/$PLUGIN.vst3" "$ROOT/ci/bin"
 
   cd "$ROOT/ci/bin"
 
   # Upload
   cd "$ROOT/ci/bin"
   zip -r ${PLUGIN}_Linux.zip $PLUGIN.so $PLUGIN.vst3 $PLUGIN.lv2
-  cp ${PLUGIN}_Linux.zip "$ROOT/bin"
 
   if [ "$BRANCH" = "release" ]; then
     curl -F "files=@${PLUGIN}_Linux.zip" "https://socalabs.com/files/set.php?key=$APIKEY"
@@ -135,29 +99,18 @@ if [ "$OS" = "linux" ]; then
 fi
 
 # Build Win version
-if [ "$OS" = "win" ]; then
-  VS_WHERE="C:/Program Files (x86)/Microsoft Visual Studio/Installer/vswhere.exe"
-  
-  MSBUILD_EXE=$("$VS_WHERE" -latest -requires Microsoft.Component.MSBuild -find "MSBuild\**\Bin\MSBuild.exe")
-  echo $MSBUILD_EXE
+if [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
+  cd "$ROOT"
 
-  cd "$ROOT/plugin/Builds/VisualStudio2022"
-  "$MSBUILD_EXE" "$PLUGIN.sln" "//p:VisualStudioVersion=16.0" "//m" "//t:Build" "//p:Configuration=Release64" "//p:Platform=x64" "//p:PreferredToolArchitecture=x64"
-  "$MSBUILD_EXE" "$PLUGIN.sln" "//p:VisualStudioVersion=16.0" "//m" "//t:Build" "//p:Configuration=Release" "//p:PlatformTarget=x86" "//p:PreferredToolArchitecture=x64"
+  cmake --preset vs
+  cmake --build --preset vs --config Release
 
   cd "$ROOT/ci/bin"
-    mkdir -p VST
-    mkdir -p VST_32
-    mkdir -p VST3
-    mkdir -p VST3_32
 
-    cp "$ROOT/plugin/Builds/VisualStudio2022/x64/Release64/VST/${PLUGIN}.dll" VST
-    cp "$ROOT/plugin/Builds/VisualStudio2022/x64/Release64/VST3/${PLUGIN}.vst3/Contents/x86_64-win/${PLUGIN}.vst3" VST3
-    cp "$ROOT/plugin/Builds/VisualStudio2022/Win32/Release/VST/${PLUGIN}.dll" VST_32
-    cp "$ROOT/plugin/Builds/VisualStudio2022/Win32/Release/VST3/${PLUGIN}.vst3/Contents/x86-win/${PLUGIN}.vst3" VST3_32
+  cp -R "$ROOT/Builds/vs/${PLUGIN}_artefacts/Release/VST/$PLUGIN.dll" "$ROOT/ci/bin"
+  cp -R "$ROOT/Builds/vs/${PLUGIN}_artefacts/Release/VST3/$PLUGIN.vst3" "$ROOT/ci/bin"
 
-  7z a ${PLUGIN}_Win.zip VST VST_32 VST3 VST3_32
-  cp ${PLUGIN}_Win.zip "$ROOT/bin"
+  7z a ${PLUGIN}_Win.zip $PLUGIN.dll $PLUGIN.vst3
 
   if [ "$BRANCH" = "release" ]; then
     curl -F "files=@${PLUGIN}_Win.zip" "https://socalabs.com/files/set.php?key=$APIKEY"
